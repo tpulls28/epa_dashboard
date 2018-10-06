34a5911f28bd9ffb057f2780c11cc9da8c2e4604
Jack Lundquist <jack.lundquist@nyu.edu>
2018-10-06 16:20:38 -0400

    Update secret_key_finder.R

--- pre_processing/secret_key_finder.R
+++ pre_processing/secret_key_finder.R
@@ -1,5 +1,3 @@
-#TESTING
-
 secret_key_finder <- function() {
   key <- readChar("C:\\Users\\Derek\\Documents\\CS_work\\Misc\\key_to_erebor.txt", file.info("C:\\Users\\Derek\\Documents\\CS_work\\Misc\\key_to_erebor.txt")$size)
   return(key)
