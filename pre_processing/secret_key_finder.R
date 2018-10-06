4d509089f2623920b4008564c5640039e7eed712
jack-lundquist <31962755+jack-lundquist@users.noreply.github.com>
2018-10-06 14:39:54 -0400

    testing

--- pre_processing/secret_key_finder.R
+++ pre_processing/secret_key_finder.R
@@ -1,3 +1,5 @@
+#TESTING
+
 secret_key_finder <- function() {
   key <- readChar("C:\\Users\\Derek\\Documents\\CS_work\\Misc\\key_to_erebor.txt", file.info("C:\\Users\\Derek\\Documents\\CS_work\\Misc\\key_to_erebor.txt")$size)
   return(key)
